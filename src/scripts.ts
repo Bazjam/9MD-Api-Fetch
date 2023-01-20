cbd05f90485f219d667db6a751cacc794aab6abc
Bazjam <bonusystem@gmail.com>
2023-01-21 00:13:59 +0200

    Finished

--- src/scripts.ts
+++ src/scripts.ts
@@ -20,33 +20,35 @@ interface ArrOfObj {
 
 const wrapper = document.querySelector("[data-insert-cards]");
 const btn = document.querySelector("[data-btn]");
-const dataUrl = "https://rickandmortyapi.com/api/character/?page=1"
+const dataUrl = "https://rickandmortyapi.com/api/character/?page="
+let counter = 1;
 
-document.addEventListener("DOMContentLoaded", function () {
-  getData();
+document.addEventListener("DOMContentLoaded", function () {   //when Web Page Start, load getData(counter)
+  getData(counter);
 });
 
+btn.addEventListener("click", () => getData(counter));  //  button click + getData
 
-
-btn.addEventListener("click", () => getData());
-
-const getData = () => {
-  fetch("https://rickandmortyapi.com/api/character/?page=1")
+const getData = (page: number) => {               // 
+  fetch(dataUrl + page)                           // fetch() 
   .then((res) => res.json())
 
   .then((data) => {
-    const arr: ArrOfObj[] = data.results;
-    arr.forEach((el) => {
-      const card = document.createElement("div");
-      card.classList.add("card-block");
-      card.innerHTML = `
+    const arr: ArrOfObj[] = data.results;         // create new arr only 1 of parts (data.results)
+    arr.forEach((el) => {                         // run throw the array (Object)
+      const card = document.createElement("div"); // create div in HTML
+      card.classList.add("card-block");           // add class 
+      // push all in HTML
+      card.innerHTML = `                          
         <img class="img-responsive" src=${el.image}>
         <p class="bold text-style">${el.name}</p>
         <p class="bold">${el.species} - ${el.status}</p>
         <p class="bold">Origins: ${el.origin.name}</p>
         <p class="bold">location: ${el.location.name}</p>`;
-      wrapper.append(card);
-      console.log(el);
+      wrapper.append(card);// append() add ich card (forEach - element) in section
+      counter++                                   // page number 2, 3, 4 ...
+      // console.log(el);
+
     });
   });
 
