89e2c5d86bc644119232c268f58bfc069ca2acdf
Bazjam <bonusystem@gmail.com>
2023-02-25 18:20:58 +0200

    Small changes

--- src/scripts.ts
+++ src/scripts.ts
@@ -31,25 +31,24 @@ btn.addEventListener("click", () => getData(counter));  //  button click + getDa
 
 const getData = (page: number) => {               // 
   fetch(dataUrl + page)                           // fetch() 
-  .then((res) => res.json())
+    .then((res) => res.json())
 
-  .then((data) => {
-    const arr: ArrOfObj[] = data.results;         // create new arr only 1 of parts (data.results)
-    arr.forEach((el) => {                         // run throw the array (Object)
-      const card = document.createElement("div"); // create div in HTML
-      card.classList.add("card-block");           // add class 
-      // push all in HTML
-      card.innerHTML = `                          
-        <img class="img-responsive" src=${el.image}>
-        <p class="bold text-style">${el.name}</p>
-        <p class="bold">${el.species} - ${el.status}</p>
-        <p class="bold">Origins: ${el.origin.name}</p>
-        <p class="bold">location: ${el.location.name}</p>`;
-      wrapper.append(card);// append() add ich card (forEach - element) in section
-      counter++                                   // page number 2, 3, 4 ...
-      // console.log(el);
+    .then((data) => {
+      const arr: ArrOfObj[] = data.results;         // create new arr only 1 of parts (data.results)
 
+      arr.forEach((el) => {                         // run throw the array (Object)
+        const card = document.createElement("div"); // create div in HTML
+        card.classList.add("card-block");           // add class 
+        // push all in HTML
+        card.innerHTML = `                          
+          <img class="img-responsive" src=${el.image}>
+          <p class="bold text-style">${el.name}</p>
+          <p class="bold">${el.species} - ${el.status}</p>
+          <p class="bold">Origins: ${el.origin.name}</p>
+          <p class="bold">location: ${el.location.name}</p>`;
+        wrapper.append(card);// append() add ich card (forEach - element) in section
+        counter++                                   // page number 2, 3, 4 ...
+        // console.log(el);
     });
   });
-
 };
